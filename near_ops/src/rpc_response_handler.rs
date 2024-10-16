8bb8cb6c8f6b4ad0ec27f711a08690197a9a2d01
Moritz <moritz.zielke@gmail.com>
2024-10-16 16:48:20 +0200

    More fine grained tps measurement

--- near_ops/src/rpc_response_handler.rs
+++ near_ops/src/rpc_response_handler.rs
@@ -1,4 +1,6 @@
-use log::warn;
+use std::time::Instant;
+
+use log::{info, warn};
 use near_jsonrpc_client::{
     errors::JsonRpcError,
     methods::tx::{RpcTransactionError, RpcTransactionResponse},
@@ -23,19 +25,36 @@ impl RpcResponseHandler {
     }
 
     pub async fn handle_all_responses(&mut self) {
-        for i in 0..self.num_expected_responses {
+        // Start timer after receiving the first response.
+        let mut timer: Option<Instant> = None;
+
+        let mut num_received = 0;
+        while num_received < self.num_expected_responses {
             let response = match self.receiver.recv().await {
                 Some(res) => res,
                 None => {
                     warn!(
-                        "Expectet {} responses but channel closed after {i}",
+                        "Expectet {} responses but channel closed after {num_received}",
                         self.num_expected_responses
                     );
                     break;
                 }
             };
+
+            num_received += 1;
+            if timer.is_none() {
+                timer = Some(Instant::now());
+            }
+
             let rpc_response = response.expect("rpc call should succeed");
             assert_transaction_and_receipts_success(&rpc_response);
+        }
+
+        if let Some(timer) = timer {
+            info!(
+                "Received {num_received} tx responses in {:.2} seconds",
+                timer.elapsed().as_secs_f64()
+            );
         }
     }
 }
